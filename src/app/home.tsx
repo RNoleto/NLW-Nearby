84870ea24307a4d84837ba3dc21dd0c74ece703f
Ramon Noleto <devnoleto@gmail.com>
2024-12-13 11:15:08 -0300

    Aula 02 - Fim da aula 2
    Finalização das categorias e lojas parceiras

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -33,7 +33,6 @@ export default function Home() {
 
             const {data} = await api.get("/markets/category/" + category)
             setMarkets(data)
-            console.log(data)
         } catch (error) {
             console.log(error)
             Alert.alert("Locais", "Não foi possível carregar os locais.")
@@ -48,7 +47,7 @@ export default function Home() {
         fetchMarkets()
     }, [category])
 
-    return <View style={{ flex: 1 }}>
+    return <View style={{ flex: 1, backgroundColor: "#cecece" }}>
         <Categories 
             data={categories} 
             onSelect={setCategory} 
--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,16 +1,41 @@
-import { View } from "react-native";
+import { useRef } from "react";
+import { Text, useWindowDimensions } from "react-native";
+import BootomSheet, {BottomSheetFlatList} from "@gorhom/bottom-sheet"
+
 import { Place, PlaceProps } from "../place";
 
 import { s } from "./styles";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 type Props = {
     data: PlaceProps[]
 }
 
 export function Places({data} : Props){
-    return(
-        <View>
-            
-        </View>
-    )
+    const dimensions = useWindowDimensions()
+    const bottomSheetRef = useRef<BottomSheet>(null)
+
+    const snapPoints = {
+        min: 278,
+        max: dimensions.height - 128
+    }
+
+    return <BootomSheet
+        ref={bottomSheetRef}
+        snapPoints={[snapPoints.min, snapPoints.max]}
+        handleIndicatorStyle={s.indicator}
+        backgroundStyle={s.container}
+        enableOverDrag={false}
+    >
+        <BottomSheetFlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Place data={item} />}
+            contentContainerStyle={s.content}
+            ListHeaderComponent={() => (
+                <Text style={s.title}>Explore locais perto de você</Text>
+            )}
+            showsVerticalScrollIndicator={false}
+        />
+    </BootomSheet>
 }
